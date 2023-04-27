baa4f46690a1dbe75cb6a936ada755d1e867b0a5
Adarsh Raj <ahazadarsh0014@gmail.com>
2023-04-27 21:25:51 +0530

    second

--- Controllers/TranslateController.js
+++ Controllers/TranslateController.js
@@ -5,12 +5,7 @@ export const translateData=async(req,res)=>{
         try {
           const {obj,code}=req.body
           console.log(obj);
-          const data=await translate(obj, {to: code}).then(resp => {
-              return resp
-          }).catch(err => {
-              return err
-          })
-  
+          const data=await translate(obj, {to: code})
           res.status(200).json({data})
         } catch (error) {
           res.status(500).json(error)
